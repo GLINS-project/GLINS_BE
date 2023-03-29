b40b8a9c742e3af08c8ef82c40deddcb50539039
lildev1-admin <skap0329@naver.com>
2023-03-29 16:20:57 +0900

    GLINS BE 프로젝트 시작

---
+++ src/main/java/GLINS_BE/GLINS/GlinsApplication.java
@@ -0,0 +1,11 @@
+package GLINS_BE.GLINS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlinsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GlinsApplication.class, args);
+	}
+}
---
+++ src/test/java/GLINS_BE/GLINS/GlinsApplicationTests.java
@@ -0,0 +1,13 @@
+package GLINS_BE.GLINS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
