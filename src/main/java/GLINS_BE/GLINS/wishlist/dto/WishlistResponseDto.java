7e223b4d08be0658ecf249dbd473a9dcd3e4c332
quiet-honey <skap0329@naver.com>
2023-11-12 01:22:54 +0900

    :fire: 위시리스트 등록 중 사용하지 않는 wishlistKind 에 관련된 코드 및 클래스 모두 제거

--- src/main/java/GLINS_BE/GLINS/wishlist/dto/WishlistResponseDto.java
+++ src/main/java/GLINS_BE/GLINS/wishlist/dto/WishlistResponseDto.java
@@ -13,13 +13,11 @@ import java.math.BigDecimal;
 public class WishlistResponseDto {
     private Long wishlist_id;
     private Long place_id;
-    private String wishlist_kind;
     private Long client_id;
 
     public WishlistResponseDto(Wishlist wishlist) {
         wishlist_id = wishlist.getWishlist_id();
         place_id = wishlist.getPlace().getId();
-        wishlist_kind = wishlist.getWishlist_kind();
         client_id = wishlist.getClient().getId();
     }
 }
